314050cf89427e6b3b9c6da4ca8f7571be23e8b0
aginsberger-cpu <aginsberger@gmail.com>
2026-02-19 22:07:48 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>אבן תקווה - האפליקציה שלי</h1>
+      <p>עכשיו זה עובד!</p>
+    </div>
+  );
+}
+
+export default App;
